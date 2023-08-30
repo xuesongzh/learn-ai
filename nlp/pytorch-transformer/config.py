bd82a08cbd269a94ef595b7c3b5e03a2e0680565
zxsong <zxsong13@gmail.com>
2023-08-31 07:08:20 +0800

    modify transformer code

--- nlp/pytorch-transformer/config.py
+++ nlp/pytorch-transformer/config.py
@@ -1,7 +1,7 @@
-import torch 
+import torch
 
 # 设备
-DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # 最长序列（受限于postition emb）
-SEQ_MAX_LEN=5000
+SEQ_MAX_LEN = 5000
